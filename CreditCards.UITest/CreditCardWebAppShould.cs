d43ebc7fb010a5571cd26c43c9f11e6e40828b1b
Erick Kurniawan <erick.kurniawan@gmail.com>
2025-06-19 10:11:02 +0700

    Add project files.

---
+++ CreditCards.UITest/CreditCardWebAppShould.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditCards.UITest
+{
+
+    public class CreditCardWebAppShould
+    {
+        [Fact]
+        public void LoadApplicationPage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://localhost:7014/");
+
+            }
+        }
+
+    }
+}
---
+++ CreditCards/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreditCards.Controllers
+{
+    public class HomeController : Controller
+    {
+        [ResponseCache(NoStore = true)]
+        public IActionResult Index()
+        {
+            string[] randomGreetings = { "Hi", "Hey", "Yo" };
+
+            int rndGreetingIndex = new Random().Next(0, randomGreetings.Length);
+
+            ViewData["RandomGreeting"] = randomGreetings[rndGreetingIndex];
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Demo application for UI testing course.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
